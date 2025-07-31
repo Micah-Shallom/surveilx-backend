b70ce719bf00c2590938f1584fda96af853c8014
Shallom Micah Bawa <micahshallom@gmail.com>
2025-07-31 13:28:43 +0100

    changes

--- models/utilty.go
+++ models/utilty.go
@@ -7,7 +7,6 @@ import (
 )
 
 func CheckExists(db *gorm.DB, receiver any, query any, args ...any) bool {
-
 	tx := db.Where(query, args...).First(receiver)
 	return !errors.Is(tx.Error, gorm.ErrRecordNotFound)
 }
