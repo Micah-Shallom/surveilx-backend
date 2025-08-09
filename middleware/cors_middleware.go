6f4b3f270d8eb9732b70e6d3b8b868d42ef22ac9
Shallom Micah Bawa <micahshallom@gmail.com>
2025-08-09 13:55:03 +0100

    changes

--- middleware/cors_middleware.go
+++ middleware/cors_middleware.go
@@ -4,50 +4,82 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// func CORSMiddleware() gin.HandlerFunc {
+//     return func(c *gin.Context) {
+//         origin := c.Request.Header.Get("Origin")
+//         if origin != "" {
+//             c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
+//         }
+//         c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+//         c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+//         c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE, PATCH")
+
+//         if c.Request.Method == "OPTIONS" {
+//             c.AbortWithStatus(204)
+//             return
+//         }
+//         c.Next()
+//     }
+// }
+
 func CORSMiddleware() gin.HandlerFunc {
     return func(c *gin.Context) {
         origin := c.Request.Header.Get("Origin")
-        if origin != "" {
-            c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
+
+        allowedOrigins := []string{
+            "http://localhost:3000",
+            "https://abunrfedusec.com",
+            "*",
         }
+
+        for _, o := range allowedOrigins {
+            if o == origin {
+                c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
+                // c.Writer.Header().Set("Vary", "Origin") // important for caching
+                break
+            }
+        }
+
         c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-        c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+        c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, Accept, Origin, Cache-Control, X-Requested-With")
         c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE, PATCH")
 
         if c.Request.Method == "OPTIONS" {
             c.AbortWithStatus(204)
             return
         }
+
         c.Next()
     }
 }
 
-func CORSMiddlewareProduction(allowedOrigins []string) gin.HandlerFunc {
-	return gin.HandlerFunc(func(c *gin.Context) {
-		origin := c.Request.Header.Get("Origin")
 
-		// Check if origin is in allowed origins
-		allowed := false
-		for _, allowedOrigin := range allowedOrigins {
-			if origin == allowedOrigin {
-				allowed = true
-				break
-			}
-		}
+// func CORSMiddlewareProduction(allowedOrigins []string) gin.HandlerFunc {
+// 	return gin.HandlerFunc(func(c *gin.Context) {
+// 		origin := c.Request.Header.Get("Origin")
 
-		if allowed {
-			c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
-		}
+// 		// Check if origin is in allowed origins
+// 		allowed := false
+// 		for _, allowedOrigin := range allowedOrigins {
+// 			if origin == allowedOrigin {
+// 				allowed = true
+// 				break
+// 			}
+// 		}
 
-		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE, PATCH")
+// 		if allowed {
+// 			c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
+// 		}
 
-		if c.Request.Method == "OPTIONS" {
-			c.AbortWithStatus(204)
-			return
-		}
+// 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+// 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+// 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE, PATCH")
 
-		c.Next()
-	})
-}
+// 		if c.Request.Method == "OPTIONS" {
+// 			c.AbortWithStatus(204)
+// 			return
+// 		}
+
+// 		c.Next()
+// 	})
+// }
